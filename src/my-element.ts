f07fa220476d6a071cc9e1b617a1eec673f453f2
Jared Ostyn <ostyn@live.com>
2023-10-23 23:00:26 +0100

    Moving to component, adding ignores

--- src/my-element.ts
+++ src/my-element.ts
@@ -25,6 +25,7 @@ export class MyElement extends LitElement {
 
   render() {
     return html`
+      <h1>Vite + Lit</h1>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src=${viteLogo} class="logo" alt="Vite logo" />

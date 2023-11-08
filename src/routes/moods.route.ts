185ba426271dacdc1b75fe2a9b04f07256380026
Jared Ostyn <ostyn@live.com>
2023-11-08 00:44:24 +0000

    Testing a new calendar package

--- src/routes/moods.route.ts
+++ src/routes/moods.route.ts
@@ -1,10 +1,14 @@
 import { css, html } from 'lit';
 import { customElement } from 'lit/decorators.js';
 import { MobxLitElement } from '@adobe/lit-mobx';
+import { AmpPlugin, easepick } from '@easepick/bundle';
 import { base } from '../baseStyles';
 import { ActionSheetController } from '../components/action-sheets/action-sheet-controller';
 import { Mood } from '../interfaces/mood.interface';
 import { moods } from '../stores/moods.store';
+import { settings } from '../stores/settings.store';
+import customCss from './test.css?url';
+import cssUrl from '/node_modules/@easepick/bundle/dist/index.css?url';
 
 @customElement('moods-route')
 export class MoodsRoute extends MobxLitElement {
@@ -13,6 +17,53 @@ export class MoodsRoute extends MobxLitElement {
             type: 'moodEdit',
             data: mood || {},
         });
+    }
+    protected firstUpdated(): void {
+        const dates = [
+            '2023-11-03',
+            '2023-11-07',
+            '2023-11-08',
+            '2023-11-11',
+            '2023-11-15',
+            '2023-11-16',
+            '2023-11-17',
+            '2023-11-21',
+        ];
+        new easepick.create({
+            element: this.shadowRoot?.getElementById('dateinput'),
+            css: [customCss, cssUrl],
+            plugins: [AmpPlugin],
+            inline: true,
+            AmpPlugin: {
+                dropdown: { months: true, years: true },
+                darkMode: settings.isDark,
+            },
+            setup(picker: any) {
+                // generate random prices
+                const randomInt = (min: number, max: number) => {
+                    return Math.floor(Math.random() * (max - min + 1) + min);
+                };
+                const prices = {} as any;
+                dates.forEach((x) => {
+                    prices[x] = randomInt(50, 200);
+                });
+
+                // add price to day element
+                picker.on('view', (evt: any) => {
+                    const { view, date, target } = evt.detail;
+                    const d = date ? date.format('YYYY-MM-DD') : null;
+
+                    if (view === 'CalendarDay' && prices[d]) {
+                        const span =
+                            target.querySelector('.day-price') ||
+                            document.createElement('span');
+                        span.className = 'day-price';
+                        span.innerHTML = `😆${prices[d]}`;
+                        target.append(span);
+                    }
+                });
+            },
+        } as any);
     }
     render() {
         return html` <article>
@@ -29,6 +80,7 @@ export class MoodsRoute extends MobxLitElement {
                     <feather-icon name="plus-circle"></feather-icon>
                 </span>
             </section>
+            <input style="display:none" id="dateinput" />
         </article>`;
     }
     static styles = [

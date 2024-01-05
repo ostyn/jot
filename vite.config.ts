a2863a07cd6e7da33b738b221ca3a4a2a16c2fa6
Jared Ostyn <ostyn@live.com>
2024-01-05 21:38:28 +0000

    Updating the start URL

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
             manifest: {
                 name: 'Jot',
                 short_name: 'Jot',
+                start_url: '/entries',
                 shortcuts: [
                     {
                         name: 'New Entry',

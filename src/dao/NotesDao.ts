760b18c4afbdd7c9c71d254f49fd54ce9e04d581
Jared Ostyn <ostyn@live.com>
2025-08-11 21:13:09 +0100

    Grab ending hashtags as the folder

--- src/dao/NotesDao.ts
+++ src/dao/NotesDao.ts
@@ -1,8 +1,29 @@
+import { EditTools } from '../interfaces/entry.interface';
+import { Note } from '../interfaces/note.interface';
 import { DexieDao } from './DexieDao';
 
 export class NotesDao extends DexieDao {
     constructor() {
         super('notes');
+    }
+    saveItem(passedEntry: Note): Promise<any> {
+        passedEntry = this.updateItemPath(passedEntry);
+        return super.saveItem(passedEntry);
+    }
+    saveItems(passedItems: any[], importTool: EditTools): Promise<any> {
+        passedItems.forEach((item) => {
+            this.updateItemPath(item);
+        });
+        return super.saveItems(passedItems, importTool);
+    }
+    private updateItemPath(item: Note): Note {
+        if (item.content && item.content.length > 0) {
+            const lastLine = item.content.split('\n').pop();
+            if (lastLine?.startsWith('#')) {
+                item.path = lastLine.substring(1).trim();
+            }
+        }
+        return item;
     }
 }
 export const notesDao = new NotesDao();

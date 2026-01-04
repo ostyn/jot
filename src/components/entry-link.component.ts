51c2e6bdcc0d7149596e566ea1d8459a5d9bdb1a
Jared Ostyn <ostyn@live.com>
2026-01-04 22:17:24 +0000

    Cleanup

--- src/components/entry-link.component.ts
+++ src/components/entry-link.component.ts
@@ -4,9 +4,7 @@ import { base } from '../baseStyles';
 import { entryDao } from '../dao/EntryDao';
 import { Entry } from '../interfaces/entry.interface';
 import { Mood } from '../interfaces/mood.interface';
-import { go } from '../routes/route-config';
 import { moods } from '../stores/moods.store';
-import { DateHelpers } from '../utils/DateHelpers';
 
 @customElement('entry-link')
 export class EntryLinkComponent extends LitElement {
